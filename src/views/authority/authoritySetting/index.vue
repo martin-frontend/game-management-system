7189d63eb324364deb6735fcd605927717beec77
Adam <adam@wanlian.tw>
2020-11-27 17:25:31 +0800

    更新管理帳號與權限設定頁面

---
+++ src/views/authority/authoritySetting/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="page-container">
+    <div class="table-container">
+      <el-tag>權限</el-tag>
+      <el-button icon="el-icon-plus" type="primary" circle style="float: right" @click="add" />
+      <el-tabs v-model="activeName" style="margin-top:10px;">
+        <el-tab-pane v-for="item in tabMapOptions" :key="item.key" :label="item.label" :name="item.key">
+          <template v-if="activeName === 'a'">
+            <p>權限</p>
+          </template>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+    <Dialog ref="dialog" />
+  </div>
+</template>
+
+<script>
+import Dialog from './dialog'
+export default {
+  name: 'AuthoritySetting',
+  components: { Dialog },
+  data() {
+    return {
+      tabMapOptions: [
+        { label: '權限', key: 'a' }
+      ],
+      activeName: 'a'
+    }
+  },
+  computed: {
+  },
+  created() {
+
+  },
+  methods: {
+    add() {
+      this.$refs.dialog.handleOpen()
+    }
+  }
+}
+</script>
---
+++ src/views/authority/manageAccount/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="page-container">
+    <div class="table-container">
+      <el-tag>後台帳號</el-tag>
+      <el-button icon="el-icon-plus" type="primary" circle style="float: right" @click="add" />
+      <el-tabs v-model="activeName" style="margin-top:10px;">
+        <el-tab-pane v-for="item in tabMapOptions" :key="item.key" :label="item.label" :name="item.key">
+          <template v-if="activeName === 'a'">
+            <p>後台帳號</p>
+          </template>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+    <Dialog ref="dialog" />
+  </div>
+</template>
+
+<script>
+import Dialog from './dialog'
+export default {
+  name: 'ManageAccount',
+  components: { Dialog },
+  data() {
+    return {
+      tabMapOptions: [
+        { label: '後台帳號', key: 'a' }
+      ],
+      activeName: 'a'
+    }
+  },
+  computed: {
+  },
+  created() {
+
+  },
+  methods: {
+    add() {
+      this.$refs.dialog.handleOpen()
+    }
+  }
+}
+</script>
