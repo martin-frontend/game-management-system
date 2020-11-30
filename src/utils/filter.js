2f81cb2501280707103f3be564c48d60bbf30755
William <m0361001@gm.ncue.edu.tw>
2020-11-30 15:03:57 +0800

    加上時間篩選組件

---
+++ src/utils/filter.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+moment.locale('zh-cn')
+
+Vue.filter('moment', function(dataStr, pattern = 'YYYY-MM-DD HH:mm:ss') {
+  return moment(dataStr).format(pattern)
+})
