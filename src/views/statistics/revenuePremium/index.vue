050bd149c105b368a1c3d9fb9348154254b0fd96
William Lin <m0361001@gm.ncue.edu.tw>
2020-12-01 15:28:30 +0800

    完成營收付費搜尋列功能

---
+++ src/views/statistics/revenuePremium/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page-container">
+    <search-panel @onType="handleType" />
+    <div class="table-container">
+      <el-tag>營收付費</el-tag>
+      <tablePanel />
+    </div>
+  </div>
+</template>
+<script>
+import searchPanel from './searchPanel'
+import tablePanel from './tablePanel'
+export default {
+  name: 'Index',
+  components: { searchPanel, tablePanel },
+  data() {
+    return {
+      type: ''
+    }
+  },
+  methods: {
+    handleType(val) {
+      this.type = val
+    }
+  }
+}
+</script>
---
+++ src/views/statistics/revenuePremium/searchPanel.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="search-panel">
+    <el-tag>請輸入查詢條件</el-tag>
+    <div style="padding: 5px 0"></div>
+    <el-form ref="form" :inline="true" :model="formData" :rules="rules">
+      <el-form-item prop="startDate">
+        <el-date-picker
+          v-model="formData.startDate"
+          type="date"
+          placeholder="選擇開始日期"
+        />
+      </el-form-item>
+      <el-form-item prop="endDate">
+        <el-date-picker
+          v-model="formData.endDate"
+          type="date"
+          placeholder="選擇結束日期"
+        />
+      </el-form-item>
+      <el-button type="primary" @click="handleSearch">查詢</el-button>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SearchPanel',
+  data() {
+    return {
+      formData: {},
+      rules: {
+        startDate: [
+          { type: 'date', required: true, message: '日期錯誤', trigger: 'change' }
+        ],
+        endDate: [
+          { type: 'date', required: true, message: '日期錯誤', trigger: 'change' }
+        ]
+      },
+      options: [
+        { value: 'all', label: 'ALL' },
+        { value: 'android', label: 'Android' },
+        { value: 'ios', label: 'iOS' }
+      ]
+    }
+  },
+  methods: {
+    handleSearch() {
+      this.$refs['form'].validate((valid, err) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    handleChange(val) {
+      this.$emit('onType', val)
+    },
+    checkDate(rule, value, callback) {
+      console.log(rule, value)
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+</style>
