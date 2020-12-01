e7e600655604e04dcd020b776b15082d42fb4cd3
unknown <Adam>
2020-12-01 11:06:16 +0800

    完成活動序號頁面表格顯示

---
+++ src/views/operation/activityCode/code.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <el-table
+      :data="tableData"
+      border
+    >
+      <el-table-column
+        prop="operating"
+        label="功能"
+        width="180"
+      >
+        <el-button type="primary" size="small">瀏覽</el-button>
+        <el-button type="primary" size="small">查看序號</el-button>
+        <el-button type="danger" size="small">刪除</el-button>
+      </el-table-column>
+      <el-table-column
+        prop="id"
+        label="編號"
+        width="180"
+      />
+      <el-table-column
+        prop="title"
+        label="標題"
+        width="180"
+      />
+      <el-table-column
+        prop="type"
+        label="類型"
+        width="180"
+      />
+      <el-table-column
+        prop="amount"
+        label="數量"
+        width="180"
+      />
+      <el-table-column
+        prop="status"
+        label="狀態"
+        width="180"
+      />
+      <el-table-column
+        prop="startTime"
+        label="開始時間"
+      />
+      <el-table-column
+        prop="endTime"
+        label="結束時間"
+      />
+      <el-table-column
+        prop="creator"
+        label="建立者"
+      />
+      <el-table-column
+        prop="content"
+        label="內容"
+      />
+    </el-table>
+    <div class="table-pagination">
+      <el-pagination
+        :current-page="1"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="tableData.length"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Item',
+  props: {
+    tableData: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.el-pagination{
+  width: 100%;
+  text-align: right;
+}
+</style>
