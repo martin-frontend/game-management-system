d8d9a0bdeeadaa8d4c6463010d4dc40347cf488a
unknown <Adam>
2020-12-01 11:08:53 +0800

    新增 filter moment isValid

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -2,5 +2,5 @@ import Vue from 'vue'
 import moment from 'moment'
 
 Vue.filter('moment', function(dataStr, pattern = 'YYYY-MM-DD HH:mm:ss') {
-  return moment(dataStr).format(pattern)
+  return moment(dataStr).isValid() ? moment(dataStr).format(pattern) : ''
 })
