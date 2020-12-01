2dc0ef41e6a49e0c7d00fa3560f11d3005065cbd
William Lin <m0361001@gm.ncue.edu.tw>
2020-12-01 10:49:42 +0800

    完成留存統計功能和補上下載按鈕

---
+++ src/views/statistics/remainStatistics/tablePanel.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <i class="download-icon el-icon-download"></i>
+    <el-tabs v-model="statistic" @tab-click="handleClick">
+      <el-tab-pane
+        v-for="item in statisticOptions"
+        :key="item.value"
+        :label="item.label"
+        :name="item.value"
+      /></el-tabs>
+    <tableData :statistic="statistic" />
+  </div>
+</template>
+<script>
+import tableData from './tableData'
+export default {
+  name: 'TablePanel',
+  components: { tableData },
+  data() {
+    return {
+      statistic: 'value',
+      statisticOptions: [
+        { value: 'value', label: '數值' },
+        { value: 'percent', label: '百分比' }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.download-icon{
+  width: 100%;
+  text-align: right;
+}
+</style>
