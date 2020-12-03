8d6afa0b6468375946496360bf09a203cd4ea7e1
WeiRenLin <william@primestar.plus>
2020-12-03 18:06:51 +0800

    表格加'上合計

--- src/views/statistics/addAccount/tableData.vue
+++ src/views/statistics/addAccount/tableData.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-table :data="group.tableData">
+    <el-table :data="group.tableData" show-summary sum-text="新增帳戶數">
       <el-table-column prop="date" label="日期" />
       <el-table-column prop="amount" label="數量" />
     </el-table>
