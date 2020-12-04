3cf99128f0173c797030be94f8b16abc21f10912
Adam <adam@primestar.plus>
2020-12-04 10:37:56 +0800

    完成發送物品 getItem

--- src/views/operation/deliverItem/index.vue
+++ src/views/operation/deliverItem/index.vue
@@ -5,15 +5,7 @@
       <el-button icon="el-icon-plus" type="primary" circle style="float: right" @click="add" />
       <el-tabs v-model="activeName" style="margin-top:10px;">
         <el-tab-pane v-for="item in tabMapOptions" :key="item.key" :label="item.label" :name="item.key">
-          <template v-if="activeName === 'all'">
-            <Item :table-data="tableData" />
-          </template>
-          <template v-if="activeName === 'notSend'">
-            <Item :table-data="tableData" />
-          </template>
-          <template v-if="activeName === 'sent'">
-            <Item :table-data="tableData" />
-          </template>
+          <Item :table-data="filterData(tableData)" />
         </el-tab-pane>
       </el-tabs>
     </div>
@@ -24,6 +16,7 @@
 <script>
 import Item from './item'
 import Dialog from './dialog'
+import { getItem } from '@/api/item'
 export default {
   name: 'DeliverItem',
   components: { Dialog, Item },
@@ -46,27 +39,65 @@ export default {
   },
   computed: {
   },
-  watch: {
-    activeName: function(val) {
-      if (val === 'notSend') {
-        this.tableData = this.initData.filter(function(item, index, array) {
-          return item.status === '未發送'
-        })
-      } else if (val === 'sent') {
-        this.tableData = this.initData.filter(function(item, index, array) {
-          return item.status === '已發送'
-        })
-      } else if (val === 'all') {
-        this.tableData = this.initData
-      }
-    }
+  // watch: {
+  //   activeName: function(val) {
+  //     if (val === 'notSend') {
+  //       this.tableData = this.initData.filter(function(item, index, array) {
+  //         return item.status === '未發送'
+  //       })
+  //     } else if (val === 'sent') {
+  //       this.tableData = this.initData.filter(function(item, index, array) {
+  //         return item.status === '已發送'
+  //       })
+  //     } else if (val === 'all') {
+  //       this.tableData = this.initData
+  //     }
+  //   }
+  // },
+  mounted() {
+    this.initdata()
   },
-  created() {
-    this.tableData = this.initData
-  },
+  // created() {
+  //   this.tableData = this.initData
+  // },
   methods: {
     add() {
       this.$refs.dialog.handleOpen()
+    },
+    filterData() {
+      switch (this.activeName) {
+        case 'notSend':
+          return this.tableData.filter(function(item, index, array) {
+            return item.status === '未發送'
+          })
+        case 'sent':
+          return this.tableData.filter(function(item, index, array) {
+            return item.status === '已發送'
+          })
+        case 'all':
+          return this.tableData
+      }
+    },
+    initdata() {
+      let state = ''
+      switch (this.activeName) {
+        case 'notSend':
+          state = '0'
+          break
+        case 'sent':
+          state = '1'
+          break
+      }
+      const formData = new FormData()
+      formData.append('state', state)
+      getItem(formData)
+        .then((response) => {
+          this.tableData = [...response.data]
+          console.log(this.tableData)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     }
   }
 }
