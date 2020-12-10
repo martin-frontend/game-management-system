b1de7370847e0d51a03eb54d330d2a547fdb8c71
WeiRenLin <m0361001@gm.ncue.edu.tw>
2020-12-10 14:40:11 +0800

    完成上傳excel功能

--- src/views/operation/accountSuspension/addAcounts.vue
+++ src/views/operation/accountSuspension/addAcounts.vue
@@ -11,7 +11,18 @@
         >
           <el-input v-model="account.id" class="form-width form-margin" />
           <el-button v-if="index == 0" @click="addAccount">新增帳號ID</el-button>
-          <i v-if="index == 0" class="el-icon-upload2 icon"></i>
+          <el-upload
+            v-if="index == 0"
+            class="upload"
+            action=""
+            :on-change="handleUpload"
+            :before-upload="beforeUpload"
+            :show-file-list="false"
+            accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+            :auto-upload="false"
+          >
+            <i class="el-icon-upload2 icon"></i>
+          </el-upload>
           <el-button v-if="index != 0" @click.prevent="removeAccount(account)">删除</el-button>
         </el-form-item>
       </el-form>
@@ -23,8 +34,12 @@
   </div>
 </template>
 <script>
+import XLSX from 'xlsx'
+
 const cloneDeep = require('lodash.clonedeep')
+
 export default {
+  name: 'AddAccounts',
   props: {
     formData: {
       type: Object,
@@ -38,7 +53,11 @@ export default {
       dialogFormVisible: false,
       formLabelWidth: '80px',
       title: '新增多筆帳號ID',
-      initData: {}
+      initData: {},
+      excelData: {
+        header: null,
+        results: null
+      }
     }
   },
   methods: {
@@ -46,12 +65,48 @@ export default {
       this.loading = false
       this.dialogFormVisible = false
       this.setInit()
-      // this.$refs['formData'].resetFields()
     },
     handleOpen(title, row) {
       this.dialogFormVisible = true
-      // this.title = title
-      //   this.form = {}
+    },
+    handleUpload(file) {
+      const rawFile = file.raw
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.onload = e => {
+          const data = e.target.result
+          const workbook = XLSX.read(data, { type: 'array' })
+          const firstSheetName = workbook.SheetNames[0] // 只讀取第一個工作表
+          const worksheet = workbook.Sheets[firstSheetName]
+          const results = []
+          for (var key in worksheet) {
+            // v是讀取取单元格的原始值
+            if (key.indexOf('A') > -1) {
+              key[0] === '!' ? results.push({ id: worksheet[key] }) : results.push({ id: worksheet[key].v })
+            }
+          }
+          // TODO 為了之後擴充留下來的程式碼 可參考upload-excel寫法
+          // const header = this.getHeaderRow(worksheet)
+          // const results = XLSX.utils.sheet_to_json(worksheet) //轉為json
+          this.generateData({ results })
+          resolve()
+        }
+        reader.readAsArrayBuffer(rawFile)
+      })
+    },
+    generateData({ results }) {
+      this.formData.accounts = results
+    },
+    beforeUpload(file, data) {
+      console.log(file, data)
+      const isCsv = file.type === '.csv'
+      const isOpenXML = file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      const isMsExcel = file.type === 'application/vnd.ms-excel'
+      if (!isCsv && !isOpenXML && !isMsExcel) {
+        this.$message.error('格式錯誤')
+        return false
+      }
+      return (isCsv || isOpenXML || isMsExcel)
     },
     removeAccount(item) {
       var index = this.formData.accounts.indexOf(item)
@@ -97,10 +152,15 @@ export default {
 .form-width{
   width: 220px;
 }
-.icon{
+
+.upload{
+  display: inline-block;
+
+  .icon{
   margin-left: 10px;
   font-size: 20px;
   vertical-align: middle;
   cursor: pointer;
+  }
 }
 </style>
